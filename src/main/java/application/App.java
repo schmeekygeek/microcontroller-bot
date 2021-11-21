8d9aa9611300692a98f7250e94b4df1c77920640
schmeekygeek <ss6087982@gmail.com>
2021-11-22 00:46:36 +0530

    minor changes

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -1,13 +1,18 @@
 package application;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.*;
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
+public class App{
+    public static void main(String[] args) throws IOException{
+        BufferedReader bufRead = new BufferedReader(new FileReader("token.txt"));
+        
+        DiscordApi api = new DiscordApiBuilder()
+          .setToken(bufRead.readLine())
+          .login().join();
+        api.addListener(new Greet());
+        api.addListener(new Help());
+        bufRead.close();
     }
 }
